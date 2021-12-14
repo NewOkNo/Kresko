2c8e7b852f08bd8572641b75b3ab74495a44a98e
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2021-12-14 17:49:32 +0200

    API setup Added basic weather component displaying current weather

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,41 @@
 <script>
 
   import axios from 'axios'
+import BasicWeather from './components/BasicWeather.vue'
 
   export default {
     created() {
-      axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=58.59&lon=25.01&exclude=hourly,daily&appid=da5ba07233d4b06864f111f800d573d6')
-        .then(response => {
-          console.log(response)
-          this.weather = response.data
-        })
+        axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=58.59&lon=25.01&exclude=hourly,daily&appid=da5ba07233d4b06864f111f800d573d6")
+            .then(response => {
+            console.log(response);
+            this.weather = response.data;
+        });
     },
     data() {
-      return {
-        weather: ''
-      }
-    }
-  }
+        return {
+            weather: ""
+        };
+    },
+    components: { BasicWeather }
+}
 
 </script>
 
 <template>
   <!--<img alt="Vue logo" src="./assets/logo.png" />-->
 
-  <p>{{ weather }}</p>
+  <BasicWeather :data="weather.current" />
+
+  <!--<p>{{ weather }}</p>-->
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@400;600&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Jost', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
