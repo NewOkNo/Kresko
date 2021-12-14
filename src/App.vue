2b7fac5b5c7ecd473876d202fb14f09abfebf28d
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2021-12-14 18:48:09 +0200

    Added UI

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script>
 
   import axios from 'axios'
-import BasicWeather from './components/BasicWeather.vue'
+  //const axios = require('axios')
+  import BasicWeather from './components/BasicWeather.vue'
 
   export default {
     created() {
-        axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=58.59&lon=25.01&exclude=hourly,daily&appid=da5ba07233d4b06864f111f800d573d6")
+        axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=58.59&lon=25.01&exclude=hourly,daily&units=metric&appid=da5ba07233d4b06864f111f800d573d6")
             .then(response => {
             console.log(response);
             this.weather = response.data;
@@ -23,8 +24,19 @@ import BasicWeather from './components/BasicWeather.vue'
 
 <template>
   <!--<img alt="Vue logo" src="./assets/logo.png" />-->
+  <main :class="(weather.current.humidity > 50) ? 'bg-blue' : 'bg-yellow'">
+    <div class="hey">
+      <span class="mediumtext">
+        How's going today in
+      </span>
+      <span class="mediumtext bold">
+        {{ weather.timezone }} 
+      </span>
+    </div>
 
-  <BasicWeather :data="weather.current" />
+    <BasicWeather :data="weather.current" />
+
+  </main>
 
   <!--<p>{{ weather }}</p>-->
 </template>
@@ -32,10 +44,29 @@ import BasicWeather from './components/BasicWeather.vue'
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Jost:wght@400;600&display=swap');
 
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+main {
+  text-align: center;
+  display: grid;
+  place-items: center;
+  height: 100%;
+}
+
 #app {
   font-family: 'Jost', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  
+  height: 100vh;
+}
+
+.bold {
+  font-weight: 600;
 }
 </style>
--- src/components/BasicWeather.vue
+++ src/components/BasicWeather.vue
@@ -5,9 +5,54 @@
 </script>
 
 <template>
+  <div class="wrapper" :class="(data.humidity > 50) ? 'bg-blue' : 'bg-yellow'">
+    <span class="bigtext">
+      {{ data.temp }}°C
+    </span>
+    <span class="mediumtext" v-for="(weather, key) in data.weather" :key="key">
+      {{ weather.description }}
+
+      <span v-if="key != (data.weather.length - 1)">
+        and
+      </span>
+    </span>
+
+    <span class="smalltext" v-if="data.humidity > 80 && data.clouds > 80">
+      Chance of rain (80%)
+    </span>
+    <span class="smalltext" v-else>
+      Chance of rain (40%)
+    </span>
+  </div>
+
   <p>{{ data }}</p>
 </template>
 
 <style>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+  }
 
+  .bg-yellow {
+    background-color: #FFD275;
+  }
+
+  .bg-blue {
+    background-color: #acc1f8;
+  }
+
+  .bigtext {
+    font-size: 9.375rem;
+    font-weight: 600;
+    line-height: 8rem;
+  }
+
+  .mediumtext {
+    font-size: 2.75rem;
+  }
 </style>
